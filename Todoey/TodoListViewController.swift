1b8b52f518d75193e87f78c7ae002be901d8c550
Mohammed Hossain <mohammed.hossain4@baruchmail.cuny.edu>
2020-10-11 20:37:45 -0400

    Create alert with text field when user presses add button

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -59,7 +59,33 @@ class TodoListViewController: UITableViewController {
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
+    //MARK: - Add New Items
     
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        // 1. Create the alert controller
+        let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
+        
+        // 2. Action when user clicks the Add Item button
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            // What will happen once the user clicks the Add Item button on UIAlert
+            print(textField.text)
+        }
+        // 3. Grab the value from the text field and print it
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+            
+        }
+        
+        // 4. Present the alert
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        
+    }
     
+
 }
 
