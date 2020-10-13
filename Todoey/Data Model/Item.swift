6948413ef870b9bf80a61613602aab0d47851aa0
Mohammed Hossain <mohammed.hossain4@baruchmail.cuny.edu>
2020-10-13 14:36:18 -0400

    Create Item class for to do item and identify whether task is completed with Boolean

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,17 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Mohammed Hossain on 10/13/20.
+//  Copyright © 2020 Mohammed Hossain. All rights reserved.
+//
+
+import Foundation
+
+// Class for to do item and whether task is completed
+
+class Item {
+    
+    var title: String = ""
+    var done: Boolean = false
+}
